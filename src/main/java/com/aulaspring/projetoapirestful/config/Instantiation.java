d29e4a47fce2e37f14cfe5ad96cd92ef9222fbf7
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-02-20 11:34:01 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/aulaspring/projetoapirestful/config/Instantiation.java
+++ src/main/java/com/aulaspring/projetoapirestful/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.aulaspring.projetoapirestful.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.aulaspring.projetoapirestful.domain.Post;
 import com.aulaspring.projetoapirestful.domain.User;
+import com.aulaspring.projetoapirestful.repositories.PostRepository;
 import com.aulaspring.projetoapirestful.repositories.UserRepository;
 
 //Essa classe será nossa base de dados de instanciação para realizar os testes
@@ -14,18 +18,32 @@ import com.aulaspring.projetoapirestful.repositories.UserRepository;
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository UserRepository;
+	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
-		UserRepository.deleteAll(); //Limpado nossa coleção no db
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		//Limpado nossas coleções no db
+		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		UserRepository.saveAll(Arrays.asList(maria, alex, bob));
+		//Nessa aplicação os Posts que irão conter os Users
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
 		
 	}
 
