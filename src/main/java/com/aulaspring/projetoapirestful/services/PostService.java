ab653d0464dfef51131d14a757616ea9ae7a38da
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-02-22 15:25:34 -0300

    Obtendo um post por id

---
+++ src/main/java/com/aulaspring/projetoapirestful/services/PostService.java
@@ -0,0 +1,27 @@
+package com.aulaspring.projetoapirestful.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aulaspring.projetoapirestful.domain.Post;
+import com.aulaspring.projetoapirestful.repositories.PostRepository;
+import com.aulaspring.projetoapirestful.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	//Para instânciar um objeto automaticamente utilizamos a notation @Autowired
+	//Mecanismo de injeção de dependência automático do Spring
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
+	
+}
