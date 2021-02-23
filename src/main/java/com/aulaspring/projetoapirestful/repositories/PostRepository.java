84af3d93030060c55bbe01a75800d039d17644e8
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-02-23 15:05:17 -0300

    Consulta simples com @Query

--- src/main/java/com/aulaspring/projetoapirestful/repositories/PostRepository.java
+++ src/main/java/com/aulaspring/projetoapirestful/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.aulaspring.projetoapirestful.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.aulaspring.projetoapirestful.domain.Post;
@@ -10,6 +11,12 @@ import com.aulaspring.projetoapirestful.domain.Post;
 //Criamos um repository para nosso banco mongoDB, todas as operações já estão incluídas
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	//Método personalizado
+	//trocamos pattern por ?0 significa que iremos obter o primeiro parâmetro obtido no método
+	//$options: 'i' ignora maiúsculas e minúsculas
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	//Para nossa busca ignorar maiúsculas e minúsculas acrescentamos o IgnoreCase
 	List<Post> findByTitleContainingIgnoreCase(String text);
--- src/main/java/com/aulaspring/projetoapirestful/services/PostService.java
+++ src/main/java/com/aulaspring/projetoapirestful/services/PostService.java
@@ -25,7 +25,11 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
+	
+//	public List<Post> findByTitle(String text){
+//		return repo.findByTitleContainingIgnoreCase(text);
+//	}
 	
 }
